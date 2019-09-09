fde19d0f22a26c3d9a8ed70d1affb80983a3ce65
Branislav Jerinic <crni611@gmail.com>
2019-09-09 16:02:51 +0200

    cf7 after submit popup added

--- assets/js/_site/global.js
+++ assets/js/_site/global.js
@@ -41,6 +41,25 @@ const Global = {
 				smallBtn: true,
 				baseClass: customClass,
 			});
+		},
+
+		/**
+		 * @description CF7 after submit popup trigger. Structure-example ( https://www.dropbox.com/s/oyj5revbgwigzxr/download.jpg?dl=0 )
+		 * @example Global.functions.afterFormSubmit('.js-cf7-holder', 'form-is-sent', 8000);
+		 * @param {string} formHolder - form holder class (recommended/default is '.js-cf7-holder')
+		 * @param {string} sentClass - class added to form parent to trigger popup (default is 'form-is-sent')
+		 * @param {number} delay - delay time before sentClass is removed (default delay time ix 5000ms)
+		 */
+		afterFormSubmit: (formHolder = '.js-cf7-holder', sentClass = 'form-is-sent', delay = 5000) => {
+			document.addEventListener('wpcf7mailsent', (e) => {
+				const formId = e.detail.id;
+
+				$(`#${formId}`).parents(formHolder).addClass(sentClass);
+
+				setTimeout(() => {
+					$(formHolder).removeClass(sentClass);
+				}, delay);
+			}, false);
 		}
 	}
 };
