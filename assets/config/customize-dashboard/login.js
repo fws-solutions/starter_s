00954899abc44b5f6703f8a2935544b417366fa0
Branislav Jerinic <crni611@gmail.com>
2019-09-16 18:04:37 +0200

    login page completed

---
+++ assets/config/customize-dashboard/login.js
@@ -0,0 +1,3 @@
+// Adding placeholder to login form
+document.getElementById('user_login').setAttribute('placeholder', 'Username');
+document.getElementById('user_pass').setAttribute('placeholder', 'Password');
