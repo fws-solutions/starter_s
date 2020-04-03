6933a0f02810dd95963b339df94ca3461bead7ba
Nikola Topalović <nikola_topalovic@yahoo.com>
2020-04-03 18:45:25 +0200

    header changes and nav refactor

--- header.php
+++ header.php
@@ -26,51 +26,71 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'starter_s' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<a class="site-header__logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img class="site-header__logo-img" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo.png" alt="<?php bloginfo( 'name' ); ?> Logo" title="<?php bloginfo( 'name' ); ?>">
-		</a><!-- .site-branding -->
+		<div class="container">
+			<div class="site-header__container">
+				<div class="site-header__branding">
+					<a class="site-header__logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img class="site-header__logo-img" src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo.png" alt="<?php bloginfo( 'name' ); ?> Logo" title="<?php bloginfo( 'name' ); ?>">
+					</a>
+				</div>
 
-		<a href="javascript:;" class="menu-btn js-menu-btn"><span></span></a>    <!-- menu-button -->
+				<div class="site-header__nav-outer">
+					<div class="site-header__nav-inner">
+						<nav id="site-navigation" class="site-nav js-site-nav">
+							<?php
+							/*
+							 * Enable this function once it's ready to replace FE code bellow.
+							 *
+							wp_nav_menu( array(
+								'theme_location' => 'menu-1',
+								'menu_class'     => 'site-nav__list',
+								'container'      => false
+							) );
+							*/
+							?>
 
-		<nav id="site-navigation" class="main-nav js-main-nav">
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_class'     => 'main-nav__list',
-				'container'      => false
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-
-		<nav class="main-nav js-main-nav">
-			<ul class="main-nav__list">
-				<li class="menu-item">
-					<a href="#">Menu Item</a>
-				</li>
-
-				<li class="menu-item menu-item-has-children">
-					<a href="#">Menu Item 2</a>
-					<ul class="sub-menu">
-						<li class="menu-item">
-							<a href="#">Submenu Item</a>
-						</li>
-
-						<li class="menu-item menu-item-has-children">
-							<a href="#">Submenu Item 2</a>
-							<ul class="sub-menu">
+							<!--
+							This HTML structure and classes must stay the same as this is how wp_nav_menu() function
+							will render it.
+							You can add new elements in <li> or <a> tags, such as SVGs or <span>s, but you cannot rearrange
+							elements, wrap them in additional <div>s or change classes.
+							//-->
+							<ul class="site-nav__list">
 								<li class="menu-item">
-									<a href="#">Third Level Item</a>
+									<a href="#">Menu Item</a>
 								</li>
 
-								<li class="menu-item">
-									<a href="#">Third Level Item 2</a>
+								<li class="menu-item menu-item-has-children">
+									<a href="#">Menu Item 2</a>
+									<?php echo fws()->render->inlineSVG('ico-arrow-down', 'site-nav__icon js-nav-icon'); ?>
+									<ul class="sub-menu">
+										<li class="menu-item">
+											<a href="#">Submenu Item</a>
+										</li>
+
+										<li class="menu-item menu-item-has-children">
+											<a href="#">Submenu Item 2</a>
+											<?php echo fws()->render->inlineSVG('ico-arrow-down', 'site-nav__icon js-nav-icon'); ?>
+											<ul class="sub-menu">
+												<li class="menu-item">
+													<a href="#">Third Level Item</a>
+												</li>
+
+												<li class="menu-item">
+													<a href="#">Third Level Item 2</a>
+												</li>
+											</ul>
+										</li>
+									</ul>
 								</li>
 							</ul>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+						</nav><!-- #site-navigation -->
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<a href="javascript:;" class="site-header__hamburger hamburger js-menu-btn"><span></span></a>
+	</header>
 
 	<div id="content" class="site-content">
