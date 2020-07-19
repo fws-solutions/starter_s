40b1f694227869529af41d64801d5481f2fe4c0b
Nikola Topalović <nikola_topalovic@yahoo.com>
2020-07-19 23:30:59 +0200

    add comment to functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
  * Load Composer and FWS Engine
  * Please run `composer install` in the theme root folder.
  *
+ * DO NOT write any scripts here, all features should be written inside FWS directory.
+ *
  * @package fws_starter_s
  */
 if ( file_exists( get_template_directory() . '/vendor/autoload.php' ) ) {
