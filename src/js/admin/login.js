910c1081510e0f2c3bd3514a2be80d74c8e8e986
Bojan Kolano <bojan19941@hotmail.com>
2021-03-16 15:18:33 +0100

    Login and forgot password

--- src/js/admin/login.js
+++ src/js/admin/login.js
@@ -1,5 +1,13 @@
+const $ = jQuery.noConflict();
+
 'use strict';
 const Login = {
+
+	$domNav: $('#nav'),
+	classForgetmenot: '.forgetmenot',
+	idRememberme: '#rememberme',
+	idWpSubmit: '#wp-submit',
+
 	init: function() {
 		const userLogin = document.getElementById('user_login');
 		if (userLogin) {
@@ -10,6 +18,10 @@ const Login = {
 		if (userPass) {
 			userPass.setAttribute('placeholder', 'Password');
 		}
+
+		$(this.$domNav).insertAfter(this.classForgetmenot);
+		$('<span class="checkmark"></span>').insertAfter(this.idRememberme);
+		$('<span class="btn-icon-login svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M8.578 16.594L13.172 12 8.578 7.406 9.984 6l6 6-6 6z"></path></svg></span>').insertAfter(this.idWpSubmit);
 	}
 };
 
