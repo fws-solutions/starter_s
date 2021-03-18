2826b285c0bdd7c47fa639a77eae68d064914315
Bojan Kolano <bojan19941@hotmail.com>
2021-03-18 15:15:32 +0100

    Removing important and css confilcts

--- src/js/admin/login.js
+++ src/js/admin/login.js
@@ -3,7 +3,6 @@ const $ = jQuery.noConflict();
 'use strict';
 const Login = {
 
-	$domNav: $('#nav'),
 	classForgetmenot: '.forgetmenot',
 	idRememberme: '#rememberme',
 	idWpSubmit: '#wp-submit',
@@ -22,14 +21,7 @@ const Login = {
 		$('#nav').insertAfter(this.classForgetmenot);
 		$('<span class="checkmark"></span>').insertAfter(this.idRememberme);
 		$('<span class="btn-icon-login svg-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M8.578 16.594L13.172 12 8.578 7.406 9.984 6l6 6-6 6z"></path></svg></span>').insertAfter(this.idWpSubmit);
-
 	}
 };
-/*
-$(function(){
-	$('#wp-auth-check-frame').on('load', function(){
-		console.log('laod the iframe');
-	});
-}); */
 
 export default Login;
