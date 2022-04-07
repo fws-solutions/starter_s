b45ef397e75a28a5f1778f22f6224f261aea2429
Anđela Šumanski <angela@forwardslashny.com>
2022-04-07 13:50:07 +0200

    Styleguide Template

--- templates/page-styleguide.php
+++ templates/page-styleguide.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Styleguide Page
+ * Template Name: Styleguide Page Old
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -11,24 +11,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-			<!-- <div class="styleguide">
+			<div class="styleguide">
 				<div class="styleguide__scrollspy-nav js-styleguide-nav-wrap">
-					 <span class="styleguide__scrollspy-nav-title">Style Nav</span>
+					<!-- <span class="styleguide__scrollspy-nav-title">Style Nav</span>
 					<div class="styleguide-filter-input-wrap">
 						<input type="text" class="styleguide-filter-input js-styleguide-filter-input" placeholder="Search...">
 					</div> -->
-					<!-- <div class="styleguide-nav-list-holder js-styleguide-nav-list-holder">
+					<div class="styleguide-nav-list-holder js-styleguide-nav-list-holder">
 						<ul class="styleguide__scrollspy-nav-list js-styleguide-nav">
 						</ul>
-					</div> -->
+					</div>
 					<!-- <a href="javascript:;" class="styleguide__scrollspy-nav-open js-styleguide-open"><?php echo fws()->render()->inlineSVG('ico-arrow-right', 'styleguide-open-icon'); ?></a> -->
+				</div>
+
+				<div class="container">
+					<h1 class="styleguide__main-head">UI Style Guide</h1>
 				</div>
 
 				<?php
 					// Base
-					// fws()->styleguide()->styleguide_init();
+					fws()->styleguide()->styleguide_init();
 				?>
-			</div> -->
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
