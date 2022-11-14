a26bd4fee2e79c04abcfbdf195793ec2d15417e2
miroslav <miroslav@forwardslashny.com>
2022-11-14 15:57:09 +0100

    security bugfix: more on xmlrpc

--- fws/src/Theme/Security.php
+++ fws/src/Theme/Security.php
@@ -68,6 +68,7 @@ class Security
     protected static function disableXmlRpc(): void
     {
         add_filter('xmlrpc_enabled', '__return_false');
+        add_filter('xmlrpc_methods', '__return_empty_array');
     }
 
 
