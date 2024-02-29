9175fc0801a5a8a4f9b017cc51b9cd82e29a2b43
Nikola Topalović <63201808+nikolatopalovic@users.noreply.github.com>
2024-02-29 07:45:43 +0100

    update .gitignore

---
+++ template-views/blocks/banner/banner.js
@@ -0,0 +1,17 @@
+// const $ = jQuery.noConflict();
+
+'use strict';
+
+jQuery(function() {
+	const Banner = {
+		/** @description Cache dom and strings. */
+
+
+		/** @description Initialize. */
+		init: function() {
+			console.log('Banner -- WP Block');
+		}
+	};
+
+	Banner.init();
+});
---
+++ template-views/blocks/basic-block/basic-block.js
@@ -0,0 +1,17 @@
+// const $ = jQuery.noConflict();
+
+'use strict';
+
+jQuery(function() {
+	const BasicBlock = {
+		/** @description Cache dom and strings. */
+
+
+		/** @description Initialize. */
+		init: function() {
+			console.log('BasicBlock -- WP Block');
+		},
+	};
+
+	BasicBlock.init();
+});
